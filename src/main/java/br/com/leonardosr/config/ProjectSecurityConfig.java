abb8e1658bca7b8b8d24795a7ee641f2e8c4ed4a
Leonardo Ribeiro <leonardosr@outlook.com>
2024-02-20 18:56:48 -0300

    first commit

---
+++ src/main/java/br/com/leonardosr/config/ProjectSecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.leonardosr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/myAccount","/myBalance","/myCards","/myLoans").authenticated()
+				.requestMatchers("/contact","/notices").permitAll())
+				.formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+}
---
+++ src/main/java/br/com/leonardosr/controller/ContactController.java
@@ -0,0 +1,13 @@
+package br.com.leonardosr.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+	@GetMapping("/contact")
+	public String saveContactInquiryDetails(){
+		return "Inquiry details are save to the DB";
+	}
+}
